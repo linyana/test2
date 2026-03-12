dfd0308001b872c501ef1c7a50f860bef1eaec46
Rik <rik.li@silksoftware.com>
2026-03-12 16:06:56 +0800

    Fix issue #2: 做code review

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/hello', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
 console.log('Hello from Node project!');
